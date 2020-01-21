6abd2aa65719eefdbd40b8a3aa639b5e6d1082a2
Thibault Clérice <leponteineptique@gmail.com>
2020-01-21 16:37:14 +0100

    (Transformer) Fixed a module init

--- pie/models/context_embedding.py
+++ pie/models/context_embedding.py
@@ -3,10 +3,12 @@ import torch.nn as nn
 
 class ReducedTransformer(nn.Module):
     def __init__(self, transformer, transformer_dim, out_dim):
-        self.transformer = transformer
+
         self.transformer_dim: int = transformer_dim
         self.out_dim: int = out_dim
+        super(ReducedTransformer, self).__init__()
 
+        self.transformer = transformer
         self.linear = nn.Linear(self.transformer_dim, self.out_dim)
 
     def forward(self, sentences):
@@ -15,7 +17,14 @@ class ReducedTransformer(nn.Module):
         :param sentences: Batch of sentences with words
         :returns: Embedding, Sentence
         """
-
         words, sentence = self.transformer(sentences)
         words = self.linear(words)
         return words, sentence
+
+
+def ReducerFunction(transformer, linear):
+    def model(sentences):
+        words, sentence = transformer(sentences)
+        words = linear(words)
+        return words, sentence
+    return model
